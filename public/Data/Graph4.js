1a3234e4cac25f86f83d73ce624874b026d9d14a
hugues898 <hugues.christophe@epfl.ch>
2023-12-22 20:37:43 +0100

    nin

--- public/Data/Graph4.js
+++ public/Data/Graph4.js
@@ -1,7 +1,7 @@
 const Graph4 = [
     {
       title: "Distribution of victory and defeat depending on the ethnicity of the target",
-      img: '../img/graph4.jpg',
+      img: '../img/graph3.jpg',
       text: "Result of the analysis after matching the data on the propensity score of each pathway to fixed the confounders",
     },
   ];
