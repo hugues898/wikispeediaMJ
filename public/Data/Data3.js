0b3419459c06dbd4923754a5122867b1754a0145
hugues898 <hugues.christophe@epfl.ch>
2023-12-22 18:00:32 +0100

    d

--- public/Data/Data3.js
+++ public/Data/Data3.js
@@ -1,8 +1,8 @@
 const Data3 = {
   id : "1",
   title: "Does the ethnicity of the targeted celebrity have an influence on the finality of the game ? ",
-  text1: "We first assessed the difference between the games played and the ethnicity of the targeted person. The majority of targeted celebrities are white. Indeed, as presented below, this distribution is clearly unequal. This makes us realize that players are more likely to target a white person than someone of another ethnicity. This is explained on the one hand by a general culture of society turned towards the West and therefore Caucasian people but also by the greater emphasis on white people in the Wikispeedia game.\n Michael Jackson : So Wikispeedia highlights white people more than people of other ethnicities ? ",
-  text2: "It's more complicated than that Michael. We need to look deeper into the analysis to see more clearly. We therefore opted for a precise analysis of the difficulty of winning a game according to the ethnicity of the targeted personality.By focusing on the games played, no gaming bias emerged and the distribution seems proportional.",
+  text1: "We first assessed the difference between the games played and the ethnicity of the targeted person. The majority of targeted celebrities are white. Indeed, as presented below, this distribution is clearly unequal. This makes us realize that players are more likely to target a white person than someone of another ethnicity. This is explained on the one hand by a general culture of society turned towards the West and therefore Caucasian people but also by the greater emphasis on white people in the Wikispeedia game.",
+  text2: "",
 }
 
 export default Data3;
