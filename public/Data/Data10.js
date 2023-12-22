dd37726df0d90f9e53dbb7f9514588d86b9df882
hugues898 <hugues.christophe@epfl.ch>
2023-12-22 20:37:04 +0100

    k

--- public/Data/Data10.js
+++ public/Data/Data10.js
@@ -1,8 +1,8 @@
 const Data10 = {
     id : "1",
     title: "Conclusion",
-    text1: "To conclude, surprisingly it seems easier to win when the person targeted belongs to 'other ethnicities'. The origin of this bias does not seem to be due to Wikispeedia but more to general knowledge. Indeed, sentiment analysis shows no difference in the way pages are written, and the distribution of links leans towards an inequality in favour of whites people. Although the bias in the victory is in favour of others ethnicity, there is still an inequality in the representation others ethnicities people within the game and in the number of links to access these pages. Moreover, the causal analysis should be consider carefully, as only one matching was made on the basis of the propensity score. Some confounders are difficult to determine due to a lack of data, such as player level.",
-    text2: "So, if MJ wants to be seen more, it would seem that being affiliated to the 'white' ethnicity is the best choice. However, if his aim is to help the player win he should choose to belong to another ethnicity.",
+    text1: "Surprisingly, it seems easier to win when the person targeted belongs to 'other ethnicities'. The origin of this bias is not due to Wikispeedia but more to general knowledge. Indeed, sentiment analysis shows no difference in the way pages are written, and the distribution of links leans towards inequality in favor of white people. Although the bias in the victory is in favor of other ethnicities, there is still an inequality in the representation of other ethnicities people within the game and in the number of links to access these pages. Moreover, the causal analysis should be considered carefully, as only one matching was made based on the propensity score. Some confounders are difficult to determine due to a lack of data, such as player level. So, if MJ wants to be seen more, it would seem that being affiliated with the 'white' ethnicity is the best choice. However, if he aims to help the player win he should choose to belong to another ethnicity.",
+    text2: "",
   }
 
 export default Data10;
