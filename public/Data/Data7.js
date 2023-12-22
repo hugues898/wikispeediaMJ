fb33016a09613d6e9a2bb3fec5168f9ab9c810c8
hugues898 <hugues.christophe@epfl.ch>
2023-12-22 18:10:32 +0100

    l

--- public/Data/Data7.js
+++ public/Data/Data7.js
@@ -1,8 +1,8 @@
 const Data7 = {
       id : "1",
       title: "",
-      text1: "It's more complicated than that Michael. We need to look deeper into the analysis to see more clearly. There are a lot of factor that are needed to be consider, the ethnicity of the target is not the only reason",
-      text2: "why people win or lose. We therefore opted for a precise analysis of the difficulty of winning a game according to the ethnicity of the targeted personality but also considering the difficulty of the pathway and the skills of the players.",
+      text1: "It's more complicated than that Michael. We need to look deeper into the analysis to see more clearly. There are a lot of factor that are needed to be consider, the ethnicity of the target is not the only reason why people win or lose. We therefore opted for a precise analysis of the difficulty of winning a game according to the ethnicity of the targeted personality but also considering the difficulty of the pathway and the skills of the players.",
+      text2: "",
     }
 
   export default Data7;
